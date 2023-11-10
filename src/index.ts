3c545e8227f1b06d94a944b0a96fab2de3a13784
nestforces <maulanamahfud2113@gmail.com>
2023-11-10 13:58:46 +0700

    migrate prisma

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import express from "express"
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path"
+import dotenv from 'dotenv';
 
+dotenv.config({
+    path: path.resolve(__dirname, '../.env'),
+  });
+  
+  
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log((`server started on port ${port}`));
